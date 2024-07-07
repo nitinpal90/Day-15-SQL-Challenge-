74f9ec340b6d14295b653606fdf668dbee089cad
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-07-07 14:06:38 +0530

    Create (36) Hacker Rank.sql

---
+++ (36) Hacker Rank.sql
@@ -0,0 +1,5 @@
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+SELECT CO.CONTINENT, FLOOR(AVG(C.POPULATION)) FROM CITY C 
+INNER JOIN COUNTRY CO ON C.COUNTRYCODE=CO.CODE 
+GROUP BY CO.CONTINENT;
